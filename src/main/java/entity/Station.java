e57741b74493f70158e9fda672197577ae4f9aec
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-09 12:46:26 +0300

    create: station entity and map to database table

---
+++ src/main/java/entity/Station.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "stations")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Station {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "city")
+    private String city;
+}
