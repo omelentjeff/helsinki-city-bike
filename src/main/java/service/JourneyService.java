021dd6eacb833f4edb86089075bba0106c013f17
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-09 12:59:16 +0300

    feat: autowire JourneyRepository to JourneyService

--- src/main/java/service/JourneyService.java
+++ src/main/java/service/JourneyService.java
@@ -1,7 +1,16 @@
 package service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import repository.JourneyRepository;
 
 @Service
 public class JourneyService {
+
+    private final JourneyRepository journeyRepository;
+
+    @Autowired
+    public JourneyService(JourneyRepository journeyRepository) {
+        this.journeyRepository = journeyRepository;
+    }
 }
