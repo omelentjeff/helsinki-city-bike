1824d222b50a262f270b99295a340e63c1ac9a4a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 09:31:05 +0300

    feat: implement constructor autowiring for JourneyService

--- src/main/java/com/omelentjeff/citybike/controller/StationController.java
+++ src/main/java/com/omelentjeff/citybike/controller/StationController.java
@@ -1,9 +1,18 @@
 package com.omelentjeff.citybike.controller;
 
+import com.omelentjeff.citybike.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/journeys")
 public class StationController {
+
+    private final StationService stationService;
+
+    @Autowired
+    public StationController(StationService stationService) {
+        this.stationService = stationService;
+    }
 }
