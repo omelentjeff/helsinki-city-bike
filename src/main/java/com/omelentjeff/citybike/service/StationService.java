7cd5d313f67812dc78b9b6772949251fdf1eb11b
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 09:28:10 +0300

    feat: implement constructor autowiring for StationRepository and StationMapper

--- src/main/java/com/omelentjeff/citybike/service/StationService.java
+++ src/main/java/com/omelentjeff/citybike/service/StationService.java
@@ -1,7 +1,19 @@
 package com.omelentjeff.citybike.service;
 
+import com.omelentjeff.citybike.mapper.StationMapper;
+import com.omelentjeff.citybike.repository.StationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StationService {
+
+    private final StationRepository stationRepository;
+    private final StationMapper stationMapper;
+
+    @Autowired
+    public StationService(StationRepository stationRepository, StationMapper stationMapper) {
+        this.stationRepository = stationRepository;
+        this.stationMapper = stationMapper;
+    }
 }
