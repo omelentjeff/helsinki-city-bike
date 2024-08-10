2c91cbc93fac272449d04ff5578f615f6ba8ea6a
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 08:02:30 +0300

    feat: implement JourneyMapper mapping methods

--- src/main/java/mapper/JourneyMapper.java
+++ src/main/java/mapper/JourneyMapper.java
@@ -1,4 +1,15 @@
 package mapper;
 
+import dto.JourneyDTO;
+import entity.Journey;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface JourneyMapper {
+    JourneyMapper INSTANCE = Mappers.getMapper(JourneyMapper.class);
+
+    JourneyDTO toDTO(Journey employee);
+
+    Journey toEntity(JourneyDTO employeeDTO);
 }
