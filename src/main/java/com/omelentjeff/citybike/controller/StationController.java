cfb6e1468624dd848c22b479e3a765e845fd89a8
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 09:32:03 +0300

    feat: expose an endpoint to retrieve all stations

--- src/main/java/com/omelentjeff/citybike/controller/StationController.java
+++ src/main/java/com/omelentjeff/citybike/controller/StationController.java
@@ -1,7 +1,15 @@
 package com.omelentjeff.citybike.controller;
 
+import com.omelentjeff.citybike.dto.JourneyDTO;
+import com.omelentjeff.citybike.dto.StationDTO;
 import com.omelentjeff.citybike.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +22,12 @@ public class StationController {
     @Autowired
     public StationController(StationService stationService) {
         this.stationService = stationService;
+    }
+
+    @GetMapping({"/", ""})
+    public ResponseEntity<Page<StationDTO>> getAllJourneys(
+            @PageableDefault(size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        Page<StationDTO> stations = stationService.getAllStations(pageable);
+        return ResponseEntity.ok(stations);
     }
 }
