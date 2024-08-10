2330083dae8062c06d091d8185042425b4374568
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 09:05:35 +0300

    feat: implement a handler for JourneyNotFoundException

--- src/main/java/com/omelentjeff/citybike/exception/GlobalExceptionHandler.java
+++ src/main/java/com/omelentjeff/citybike/exception/GlobalExceptionHandler.java
@@ -1,7 +1,19 @@
 package com.omelentjeff.citybike.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(JourneyNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleEmployeeNotFound(JourneyNotFoundException ex) {
+        ErrorResponse error = new ErrorResponse();
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimeStamp(System.currentTimeMillis());
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
 }
