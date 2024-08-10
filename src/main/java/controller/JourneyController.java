a516d447fc1f3bb8da9140b0612029887a245b7b
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 07:19:10 +0300

    feat: add autowired construction for JourneyService

--- src/main/java/controller/JourneyController.java
+++ src/main/java/controller/JourneyController.java
@@ -1,9 +1,18 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.JourneyService;
 
 @RestController
 @RequestMapping("api/journeys")
 public class JourneyController {
+
+    private final JourneyService journeyService;
+
+    @Autowired
+    public JourneyController(JourneyService journeyService) {
+        this.journeyService = journeyService;
+    }
 }
