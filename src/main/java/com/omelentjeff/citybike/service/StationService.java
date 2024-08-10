6c36084f59980c5baccc1b22ffd03855a070e1de
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-10 09:33:10 +0300

    feat: implement a method to retrieve all stations

--- src/main/java/com/omelentjeff/citybike/service/StationService.java
+++ src/main/java/com/omelentjeff/citybike/service/StationService.java
@@ -1,9 +1,19 @@
 package com.omelentjeff.citybike.service;
 
+import com.omelentjeff.citybike.dto.JourneyDTO;
+import com.omelentjeff.citybike.dto.StationDTO;
+import com.omelentjeff.citybike.entity.Journey;
+import com.omelentjeff.citybike.entity.Station;
 import com.omelentjeff.citybike.mapper.StationMapper;
 import com.omelentjeff.citybike.repository.StationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class StationService {
@@ -15,5 +25,14 @@ public class StationService {
     public StationService(StationRepository stationRepository, StationMapper stationMapper) {
         this.stationRepository = stationRepository;
         this.stationMapper = stationMapper;
+    }
+
+    public Page<StationDTO> getAllStations(Pageable pageable) {
+        Page<Station> stationPage = stationRepository.findAll(pageable);
+        List<StationDTO> stationDTOs = stationPage.stream()
+                .map(stationMapper::toDTO)
+                .collect(Collectors.toList());
+
+        return new PageImpl<>(stationDTOs, pageable, stationPage.getTotalElements());
     }
 }
