7aa9c42592192d33aa03394f9d3e548139b5a101
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-14 18:46:36 +0300

    feat: add custom repository method for searching stations by name or address

--- src/main/java/com/omelentjeff/citybike/repository/StationRepository.java
+++ src/main/java/com/omelentjeff/citybike/repository/StationRepository.java
@@ -1,9 +1,15 @@
 package com.omelentjeff.citybike.repository;
 
 import com.omelentjeff.citybike.entity.Station;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StationRepository extends JpaRepository<Station, Long> {
+    Page<Station> findByNameContainingIgnoreCaseOrAddressContainingIgnoreCase(String name, String category, Pageable pageable);
+
 }
