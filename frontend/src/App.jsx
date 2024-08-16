dcf2353cd613d039b00ab891b3278d501588dde0
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-16 12:49:46 +0300

    feat: implement MyAppBar in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,13 @@
 import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import MyAppBar from "./MyAppBar.jsx";
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <Container>
+      <MyAppBar />
+    </Container>
+  );
 }
