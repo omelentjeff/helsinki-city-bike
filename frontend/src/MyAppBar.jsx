18d33a7ab9f91b811d95347511df4f42c3c48c66
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-16 12:49:02 +0300

    feat: create app bar component

---
+++ frontend/src/MyAppBar.jsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function MyAppBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, textAlign: "center" }}
+          >
+            Helsinki City Bike
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
