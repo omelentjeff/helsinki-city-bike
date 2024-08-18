29946cf597265006842ffcd051f39615a00a8dde
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-18 16:15:44 +0300

    feat: implement a prettier table from material ui component library

--- frontend/src/JourneyTableComponent.jsx
+++ frontend/src/JourneyTableComponent.jsx
@@ -1,38 +1,110 @@
-import React from "react";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+
+function formatToMinsAndSeconds(duration) {
+  const minutes = Math.floor((duration % 3600) / 60);
+  const seconds = duration % 60;
+  return `${minutes}m ${seconds}s`;
+}
+
+const columns = [
+  { id: "departureTime", label: "Departure Time", minWidth: 170 },
+  { id: "returnTime", label: "Return Time", minWidth: 170 },
+  { id: "departureStation", label: "Departure Station", minWidth: 170 },
+  { id: "returnStation", label: "Return Station", minWidth: 170 },
+  {
+    id: "coveredDistance",
+    label: "Covered Distance (m)",
+    minWidth: 170,
+    align: "right",
+  },
+  {
+    id: "duration",
+    label: "Duration",
+    minWidth: 170,
+    align: "right",
+    format: (value) => formatToMinsAndSeconds(value),
+  },
+];
 
 export default function JourneyTableComponent({ data }) {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
   return (
-    <TableContainer>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Departure Time</TableCell>
-            <TableCell>Return Time</TableCell>
-            <TableCell>Departure Station</TableCell>
-            <TableCell>Return Station</TableCell>
-            <TableCell>Covered Distance</TableCell>
-            <TableCell>Duration</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {data.map((row, index) => (
-            <TableRow key={index}>
-              <TableCell>{row.departureTime}</TableCell>
-              <TableCell>{row.returnTime}</TableCell>
-              <TableCell>{row.departureStation.name}</TableCell>
-              <TableCell>{row.returnStation.name}</TableCell>
-              <TableCell>{row.coveredDistance}</TableCell>
-              <TableCell>{row.duration}</TableCell>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {data
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row, index) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={index}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      if (
+                        column.id === "departureStation" ||
+                        column.id === "returnStation"
+                      ) {
+                        return (
+                          <TableCell key={column.id} align={column.align}>
+                            {value.name}
+                          </TableCell>
+                        );
+                      }
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === "number"
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={data.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
   );
 }
