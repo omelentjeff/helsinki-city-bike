e163ef10e4124e2b75153e3b79028779671e129f
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-18 16:30:47 +0300

    fix: CORS config changed server responses, fix this issue

--- backend/src/main/java/com/omelentjeff/citybike/config/WebConfig.java
+++ backend/src/main/java/com/omelentjeff/citybike/config/WebConfig.java
@@ -6,11 +6,16 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080") // Update with your frontend domain
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow HTTP methods as needed
+                .allowedHeaders("*") // Allow all headers or specify required headers
+                .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
     }
 }
+
+
