6a847a9d88e9c4ab04531382f00c3d479878c63c
Otto Melentjeff <otto.melentjeff@tuni.fi>
2024-08-20 20:01:12 +0300

    feat: create service function for fetching single entity

--- frontend/src/apiService.jsx
+++ frontend/src/apiService.jsx
@@ -1,9 +1,19 @@
-// src/apiService.js
 export const fetchData = async (endpoint, page = 0, pageSize = 10) => {
   try {
     const response = await fetch(
       `http://localhost:8080/api/${endpoint}?page=${page}&size=${pageSize}`
     );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(`Error fetching ${endpoint} data:`, error);
+    throw error;
+  }
+};
+
+export const fetchSingleData = async (endpoint, id) => {
+  try {
+    const response = await fetch(`http://localhost:8080/api/${endpoint}/${id}`);
     const data = await response.json();
     return data;
   } catch (error) {
